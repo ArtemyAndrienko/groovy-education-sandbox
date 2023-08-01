67c60a87febd234a94ab81d195939f4352a9afd5
andr <artemy.andrienko@gmail.com>
2023-08-01 23:41:34 +0300

    pre-check

--- src/main/groovy/im/educ/groovy/Main.groovy
+++ src/main/groovy/im/educ/groovy/Main.groovy
@@ -6,11 +6,21 @@ class App {
     static void main(String[] args) {
         def text = "Hello world!"
 
-        def encoded = text.bytes.encodeBase64().toString()
-        println "[ENCODED]: ${ encoded }"
-
+        def encoded = App.Hello("world!", true)
+        println "[ENCODED]: ${encoded}"
 
         byte[] decoded = encoded.decodeBase64()
-        println "[DECODED]: ${ new String(decoded)}"
+        println "[DECODED]: ${new String(decoded)}"
     }
+
+    static String Hello(String str, Boolean encoded) {
+        switch (encoded) {
+            case true:
+                def text = "Hello " + str
+                return text.bytes.encodeBase64().toString()
+            case false:
+                return "Hello " + str
+        }
+    }
+
 }
